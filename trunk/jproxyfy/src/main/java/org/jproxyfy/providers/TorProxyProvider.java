d89e4ccb3ae85408a32ffa954af3ba397b9ea995
devender.gollapally <devender.gollapally@4bc28cf4-6a80-11de-8235-5b595ee06194>
2009-07-07 09:03:54 +0000

    Minor Changes

---
+++ trunk/jproxyfy/src/main/java/org/jproxyfy/providers/TorProxyProvider.java
@@ -0,0 +1,12 @@
+package org.jproxyfy.providers;
+
+import org.jproxyfy.ipinfo.IpInfo;
+
+public class TorProxyProvider extends AbstractProxyProvider {
+
+	@Override
+	protected IpInfo rent() {
+		return null;
+	}
+
+}
