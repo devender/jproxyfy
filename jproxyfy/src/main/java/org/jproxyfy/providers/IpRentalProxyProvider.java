7564c344e8d9b9fbb3f95969304cc000dedac03f
devender.gollapally <devender.gollapally@4bc28cf4-6a80-11de-8235-5b595ee06194>
2009-07-07 09:03:54 +0000

    Minor Changes

---
+++ jproxyfy/src/main/java/org/jproxyfy/providers/IpRentalProxyProvider.java
@@ -0,0 +1,13 @@
+package org.jproxyfy.providers;
+
+import org.jproxyfy.ipinfo.IpInfo;
+
+public class IpRentalProxyProvider extends AbstractProxyProvider {
+
+	@Override
+	protected IpInfo rent() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
